1bc9d6536b3099fcc56bc26607553c471de443f4
maks_kras <maksym_krasovskiy@ya.ru>
2025-11-13 20:39:29 +0300

    black for category.py

--- src/category.py
+++ src/category.py
@@ -3,8 +3,9 @@ from src.product import Product
 
 class Category:
     """Класс для представления категории товара"""
+
     category_count: int = 0
-    product_count: int  = 0
+    product_count: int = 0
 
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         """
