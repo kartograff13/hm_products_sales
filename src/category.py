d448c894c90b133e28085bb97b01b1e0d6a8ea45
maks_kras <maksym_krasovskiy@ya.ru>
2025-11-13 20:20:13 +0300

    init task_2

--- src/category.py
+++ src/category.py
@@ -3,6 +3,8 @@ from src.product import Product
 
 class Category:
     """Класс для представления категории товара"""
+    category_count: int = 0
+    product_count: int  = 0
 
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         """
@@ -16,3 +18,10 @@ class Category:
         self.name = name
         self.description = description
         self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
+
+    def get_products_count(self) -> int:
+        """Метод для получения количества продуктов в категории"""
+        return len(self.products)
